ea1eb6f0c331857c17b1583d861610b778b3057c
Wei Lun <garyganweilun@gmail.com>
2022-04-21 02:11:18 +0800

    Set up done

---
+++ components/Layout/Body.js
@@ -0,0 +1,9 @@
+const Body = ({ children }) => {
+    return (
+        <section className="h-screen">
+            {children}
+        </section>
+    )
+}
+
+export default Body
---
+++ components/Layout/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (<div className="bg-skobeloff-200">
+        <footer className="flex flex-wrap items-center justify-between p-3 m-auto">
+            <div className="container mx-auto flex flex-col flex-wrap items-center justify-between">
+                © WhatTheFloor 2022
+            </div>
+        </footer>
+    </div>)
+}
+
+export default Footer
